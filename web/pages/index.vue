05f7554cae4ed0b0c581e7cde1012ea3d2657f8e
HammerChen <chenxiaochui@126.com>
2020-03-05 21:53:47 +0800

    前端课程列表页面

---
+++ web/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-center pa-3">
+        <h3>Video102</h3>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
