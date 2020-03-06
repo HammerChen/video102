deffbcf8f0d1cdb0e1642b6e14391c12151a8f02
HammerChen <chenxiaochui@126.com>
2020-03-06 11:29:48 +0800

    完成前端用户登录注册API

--- server/apps/server/src/main.ts
+++ server/apps/server/src/main.ts
@@ -7,9 +7,10 @@ async function bootstrap() {
   app.enableCors()
 
   const options = new DocumentBuilder()
-    .setTitle('Video102-后台管理API')
-    .setDescription('供后台管理界面调用的服务端API')
+    .setTitle('Video102-前端API')
+    .setDescription('供网站和APP调用的服务端API')
     .setVersion('1.0')
+    .addBearerAuth()
     .build()
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('api-docs', app, document)
